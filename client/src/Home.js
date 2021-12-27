a4ac8fec2f589d780aa432e0aaf4abe51082444e
Tavian <taviantaylor00@gmail.com>
2021-12-27 18:37:06 +0000

    Added welcome message

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,16 @@
+import OfferList from './OfferList';
 import useFetch from './useFetch';
+import { Typography } from '@material-ui/core';
 const Home = () => {
     const { error, isPending, data: offers } = useFetch('http://localhost:8080/soa/api/offers')
     return (
         <div>
+            <Typography variant="h6" align="center">
+                Welcome {localStorage.getItem("userId")}, see travels offers below
+            </Typography>
             { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
-            { offers && <p>{offers}</p> }
+            { offers && <OfferList offers={offers}/>}
         </div>
     )
 }
